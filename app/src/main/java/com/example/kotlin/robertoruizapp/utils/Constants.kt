eb71a43ff02ef71a627bbd630e956245370b2eaf
Ivan Rodriguez <xxivanxxy@gmail.com>
2023-10-25 20:43:13 -0600

    feature: Nav bar almost finished
    Falta crear los fragmentos correspondientes para que te envien a esa pagina que se quiere utilizar

--- app/src/main/java/com/example/kotlin/robertoruizapp/utils/Constants.kt
+++ app/src/main/java/com/example/kotlin/robertoruizapp/utils/Constants.kt
@@ -4,12 +4,14 @@ object Constants {
     const val BASE_URL_PROYECTO = "https://us-central1-robertoruiz-eca78.cloudfunctions.net/api/v1/"
     const val SPLASHCREEN_DURATION = 1000L
     const val CURSO_ID_EXTRA = "cursoExtra"
-    const val MENU_CURSOS = "CURSOS"
+    //Cambios por la nueva barra de navegación
     const val MENU_INICIO = "INICIO"
-    const val MENU_PROGRAM = "PROGRAM"
+    const val MENU_FEED = "FEED"
+    const val MENU_FRRO = "FRRO"
+    const val MENU_PERFIL = "PERFIL"
+    //No se para que se usan las siguientes constantes pero las dejo por si a caso
     const val BASE_URL = "https://us-central1-robertoruiz-eca78.cloudfunctions.net/api/v1/"
     const val ID_PROGRAM = "ID_PROGRAM"
-    const val MENU_PERFIL = "PERFIL"
     const val ID_PERFIL = "641e2e746f6fd2c83702ca33"
 
 }

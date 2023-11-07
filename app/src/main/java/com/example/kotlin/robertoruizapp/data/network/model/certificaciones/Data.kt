eebce853cb5113ebb169636755bb1224246dd5c0
Dante <ddperezp03@gmail.com>
2023-11-06 19:34:02 -0600

    documented code

--- app/src/main/java/com/example/kotlin/robertoruizapp/data/network/model/certificaciones/Data.kt
+++ app/src/main/java/com/example/kotlin/robertoruizapp/data/network/model/certificaciones/Data.kt
@@ -1,6 +1,19 @@
 package com.example.kotlin.robertoruizapp.data.network.model.certificaciones
 import com.example.kotlin.robertoruizapp.data.network.model.certificaciones.Document
 
+/*
+    * Created by Dante Perez 2/11/2023
+    * A data class representing a certification object.
+    *
+    * Parameters:
+    *  data: A Data object containing the certification data.
+    *  results: An Int representing the number of results.
+    *  status: A String representing the status of the certification object.
+    *
+    * Methods:
+    *  None.
+
+ */
 data class Data(
     val documents: List<Document>
 ) {
--- app/src/main/java/com/example/kotlin/robertoruizapp/domain/CertificacionesListRequirement.kt
+++ app/src/main/java/com/example/kotlin/robertoruizapp/domain/CertificacionesListRequirement.kt
@@ -3,6 +3,19 @@ package com.example.kotlin.robertoruizapp.domain
 import com.example.kotlin.robertoruizapp.data.network.model.certificaciones.CertificacionesObjeto
 import com.example.kotlin.robertoruizapp.data.CertificacionesRepository
 
+/*
+    * Created by Dante Perez 2/11/2023
+    * a class that represents as intermedary between the data layer and the presentation layer.
+    * It is used to get the certification data from the repository.
+    * Parameters:
+    * certificacionesRepository: A CertificacionesRepository object.
+    *
+    *
+    * Methods:
+    * invoke(): A suspend function that returns a CertificacionesObjeto object.
+    * @return a CertificacionesObjeto object containing the certification data.
+    *
+ */
 class CertificacionesListRequirement {
 
     private val certificacionesRepository = CertificacionesRepository()

2e11342d2a0717f608c65dd96b632dfe2f6ca863
Dante <ddperezp03@gmail.com>
2023-11-08 17:08:09 -0600

    fix: add security token

--- app/src/main/java/com/example/kotlin/robertoruizapp/data/CertificacionesRepository.kt
+++ app/src/main/java/com/example/kotlin/robertoruizapp/data/CertificacionesRepository.kt
@@ -5,6 +5,7 @@ import com.example.kotlin.robertoruizapp.data.network.model.NetworkModuleDICerti
 import com.example.kotlin.robertoruizapp.data.network.model.certificaciones.CertificacionesObjeto
 import com.example.kotlin.robertoruizapp.data.network.model.CertificacionApiService
 import com.example.kotlin.robertoruizapp.data.network.model.CertificacionApiClient
+import com.example.kotlin.robertoruizapp.framework.view.activities.LoginActivity
 
 /*
     * Created by Dante Perez 2/11/2023
@@ -22,15 +23,14 @@ import com.example.kotlin.robertoruizapp.data.network.model.CertificacionApiClie
 class CertificacionesRepository {
     // A service provided by a dependency injection module for network operations.
     // It is initialized lazily.
-    private lateinit var api: CertificacionApiService
+//    private lateinit var api: CertificacionApiService
+    private val api: CertificacionApiService = NetworkModuleDICertificaciones()
 
     /**
      * Fetches certification data from a remote API.
      * @return a CertificacionesObjeto object containing the certification data.
      */
-    suspend fun getCertificaciones(): CertificacionesObjeto? { // A suspending function.
-
-        api = NetworkModuleDICertificaciones() // Initializes the api property.
-        return api.getCertificaciones() // Returns a CertificacionesObjeto object.
+    suspend fun getCertificaciones(token: String): CertificacionesObjeto? { // A suspending function.
+        return api.getCertificaciones("Bearer ${LoginActivity.token}")
     }
 }

ef87f9370b2376446974772a125fe58243c9400e
unknown <urigopar889@gmail.com>
2023-11-11 13:11:12 -0600

    add: Social Networks

--- app/src/main/java/com/example/kotlin/robertoruizapp/framework/view/fragments/FragmentoFRRO.kt
+++ app/src/main/java/com/example/kotlin/robertoruizapp/framework/view/fragments/FragmentoFRRO.kt
@@ -1,10 +1,13 @@
 package com.example.kotlin.robertoruizapp.framework.view.fragments
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import com.example.kotlin.robertoruizapp.databinding.FragmentoFrroBinding
@@ -29,6 +32,44 @@ class FragmentoFRRO : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentoFrroBinding.inflate(inflater, container, false)
-        return binding.root
+        val view = binding.root
+
+
+        binding.faceBook.setOnClickListener {
+            openFacebookPage()
+        }
+
+        binding.instagram.setOnClickListener{
+            openInstagramPage()
+        }
+
+        return view
+    }
+
+    private fun openFacebookPage() {
+        val url = "https://www.facebook.com/fundacionruizobregon" // Reemplaza con la URL de tu página de Facebook
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse(url)
+
+        // Verifica si hay una aplicación que pueda manejar el intent
+        if (intent.resolveActivity(requireActivity().packageManager) != null) {
+            startActivity(intent)
+        } else {
+            // Muestra un mensaje de error o realiza una acción alternativa
+            Toast.makeText(context, "No se encontró una aplicación para abrir el enlace", Toast.LENGTH_SHORT).show()
+        }
+    }
+    private fun openInstagramPage() {
+        val url = "https://www.instagram.com/frobertoruizobregon/" // Reemplaza con la URL de tu página de Facebook
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse(url)
+
+        // Verifica si hay una aplicación que pueda manejar el intent
+        if (intent.resolveActivity(requireActivity().packageManager) != null) {
+            startActivity(intent)
+        } else {
+            // Muestra un mensaje de error o realiza una acción alternativa
+            Toast.makeText(context, "No se encontró una aplicación para abrir el enlace", Toast.LENGTH_SHORT).show()
+        }
     }
 }

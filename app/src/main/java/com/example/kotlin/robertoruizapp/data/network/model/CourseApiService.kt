f0a17cefe8552d2689dd76f9149881d30c0a9a0d
mafeermoreno <marifermg29@gmail.com>
2023-11-15 10:11:06 -0600

    Add conection to the API to obtain variables

--- app/src/main/java/com/example/kotlin/robertoruizapp/data/network/model/CourseApiService.kt
+++ app/src/main/java/com/example/kotlin/robertoruizapp/data/network/model/CourseApiService.kt
@@ -1,8 +1,10 @@
 package com.example.kotlin.robertoruizapp.data.network.model
 
 import com.example.kotlin.robertoruizapp.data.network.model.Course.CourseObject
+import com.example.kotlin.robertoruizapp.data.network.model.Course.Document
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Path
 
 /**
  * Interface that defines how requests to the Courses API should be made.
@@ -16,4 +18,8 @@ interface CourseApiService {
      */
     @GET("course")
     suspend fun getCourse(@Header("Authorization") authToken: String): CourseObject
+    // En tu CourseApiService
+    @GET("course/{id}")
+    suspend fun getCourseById(@Path("id") cursoId: String, @Header("Authorization") authToken: String): CourseObject
+
 }
--- app/src/main/java/com/example/kotlin/robertoruizapp/framework/view/fragments/FragmentoCursoDetalles.kt
+++ app/src/main/java/com/example/kotlin/robertoruizapp/framework/view/fragments/FragmentoCursoDetalles.kt
@@ -2,13 +2,26 @@ package com.example.kotlin.robertoruizapp.framework.view.fragments
 
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import com.example.kotlin.robertoruizapp.R
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.kotlin.robertoruizapp.data.CourseRepository
+import com.example.kotlin.robertoruizapp.data.network.model.Course.CourseObject
+import com.example.kotlin.robertoruizapp.data.network.model.Course.Document
 import com.example.kotlin.robertoruizapp.databinding.FragmentoCursoDetallesBinding
+import com.example.kotlin.robertoruizapp.databinding.FragmentoHomeBinding
+import com.example.kotlin.robertoruizapp.framework.view.activities.LoginActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 
 class FragmentoCursoDetalles: Fragment() {
@@ -17,12 +30,66 @@ class FragmentoCursoDetalles: Fragment() {
     private val binding get() = _binding!!
 
     @RequiresApi(Build.VERSION_CODES.N)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentoCursoDetallesBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val cursoId = arguments?.getString("CURSO_ID")
-        // Aquí puedes usar cursoId para cargar los detalles del curso
+        // Obtener el ID del curso de los argumentos
+        val cursoId = arguments?.getString("CURSO_ID") ?: return
+        Log.d("CursoDetalles", "ID del curso recibido: $cursoId")
+
+        // Obtener la información del curso con ese ID
+        getInfoCourse(cursoId)
     }
 
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun getInfoCourse(cursoId: String) {
 
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val courseRepository = CourseRepository()
+                val result: Document? = courseRepository.getCourseById(cursoId, LoginActivity.token)
+                Log.d("CursoDetalles", "Detalles del curso obtenidos: ${result?.name}")
+                withContext(Dispatchers.Main) {
+                    if (result != null) {
+                        updateUIWithCourseDetails(result)
+                    } else {
+                        showError("No se encontraron detalles del curso.")
+                    }
+                }
+            } catch (e: Exception) {
+                withContext(Dispatchers.Main) {
+                    showError("Error al obtener los detalles del curso: ${e.message}")
+                }
+                e.printStackTrace() // Log the exception
+            } finally {
+                withContext(Dispatchers.Main) {
+                }
+            }
+        }
+    }
+
+    private fun showError(message: String) {
+        // Mostrar un Toast o un diálogo con el mensaje de error
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+    }
+
+    private fun updateUIWithCourseDetails(course: Document) {
+        // Asegúrate de que estás llamando a las vistas correctas aquí
+        binding.nombreCursoInfo.text = course.name
+        // ... etc. Actualiza todas las vistas que necesites con la información del curso
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }

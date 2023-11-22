9dd59173f90be6163a0dc10be0d27a8d9c619c14
Dante <ddperezp03@gmail.com>
2023-11-22 10:34:33 -0600

    merge dev

--- app/src/main/java/com/example/kotlin/robertoruizapp/data/network/model/commentCourse/Document.kt
+++ app/src/main/java/com/example/kotlin/robertoruizapp/data/network/model/commentCourse/Document.kt
@@ -4,3 +4,10 @@ import com.example.kotlin.robertoruizapp.data.network.model.Course.CourseObject
 data class Document(
     val _id: String,
     val course: CourseObject,
+    val createdAt: String,
+    val description: String,
+    val id: String,
+    val name: String,
+    val updatedAt: String
+)
+

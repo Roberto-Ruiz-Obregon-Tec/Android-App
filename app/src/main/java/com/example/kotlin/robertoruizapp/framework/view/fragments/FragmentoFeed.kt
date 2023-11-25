d3714c0037e405d95beb86638234d7fd577c5150
unknown <urigopar889@gmail.com>
2023-11-24 21:16:03 -0600

    fix: Navbar of Feed

--- app/src/main/java/com/example/kotlin/robertoruizapp/framework/view/fragments/FragmentoFeed.kt
+++ app/src/main/java/com/example/kotlin/robertoruizapp/framework/view/fragments/FragmentoFeed.kt
@@ -1,35 +1,17 @@
 package com.example.kotlin.robertoruizapp.framework.view.fragments
 
-import android.annotation.SuppressLint
+
 import android.os.Build
 import com.example.kotlin.robertoruizapp.databinding.FragmentoFeedBinding
 import android.os.Bundle
-import android.util.Log
-import android.view.ContextThemeWrapper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.annotation.RequiresApi
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.kotlin.robertoruizapp.R
-import com.example.kotlin.robertoruizapp.data.EventRepository
-import com.example.kotlin.robertoruizapp.data.Repository
-import com.example.kotlin.robertoruizapp.data.RepositoryPublication
-import com.example.kotlin.robertoruizapp.data.companyCertificationRepository
-import com.example.kotlin.robertoruizapp.data.network.model.Events.EventObject
-import com.example.kotlin.robertoruizapp.data.network.model.companyCertification.CertificacionEmpresaObj
-import com.example.kotlin.robertoruizapp.data.network.model.publication.PublicObjeto
-import com.example.kotlin.robertoruizapp.framework.adapters.EmpresaAdapter
-import com.example.kotlin.robertoruizapp.framework.adapters.EventsAdapter
-import com.example.kotlin.robertoruizapp.framework.adapters.OnEventClickListener
-import com.example.kotlin.robertoruizapp.framework.adapters.PublicationAdapter
-import com.example.kotlin.robertoruizapp.framework.view.activities.LoginActivity
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import com.example.kotlin.robertoruizapp.databinding.FragmentoInicioBinding
 
 /**
  * This fragment shows the companies with their certifications and events
@@ -37,227 +19,66 @@ import kotlinx.coroutines.withContext
  * @property _binding Variable for the automatically generated view binding for this fragment.
  * @property binding Access property to `_binding` that ensures it is not null.
  */
-class FragmentoFeed : Fragment(), FragmentoCommentsPublication.OnBackToPublicationListener {
+class FragmentoFeed : Fragment(){
 
     private var _binding: FragmentoFeedBinding? = null
 
     private val binding get() = _binding!!
 
-    @RequiresApi(Build.VERSION_CODES.N)
-    override fun onBackToPublication() {
-        handleButton2Click()
-    }
 
-    private val onEventClickListener = object : OnEventClickListener {
-        override fun onEventClick(eventID: String) {
-            Log.d("FragmentoFeed", "Event clicked: $eventID")
-            val fragmentDetails = FragmentoInfoEventos().apply { 
-                arguments = Bundle().apply {
-                    putString("eventID", eventID)
-                }
-            }
-
-            parentFragmentManager.beginTransaction().apply {
-                replace(R.id.nav_host_fragment_content_main, fragmentDetails) 
-                addToBackStack(null)
-                commit()
-            }
-        }
-    }
-    private val onCommentClickListener = object : PublicationAdapter.OnCommentClickListener {
-        override fun OnCommentClicked(publicationId: String) {
-            val fragmentoCommentDetalles = FragmentoCommentsPublication().apply {
-                arguments = Bundle().apply {
-                    putString("PUBLICACION_ID", publicationId)
-                }
-            }
-
-            parentFragmentManager.beginTransaction()
-                .replace(R.id.nav_host_fragment_content_main, fragmentoCommentDetalles)
-                .addToBackStack(null)
-                .commit()
-        }
-    }
-    
-    /**
-     * Create the fragment view.
-     *
-     *
-     * @param inflater The LayoutInflater used to inflate the fragment.
-     * @param container The container where the fragment will be inserted.
-     * @param savedInstanceState A Bundle containing data from the previous state of the fragment.
-     * @return The root view of the inflated fragment.
-     */
-
-    @RequiresApi(Build.VERSION_CODES.N)
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentoFeedBinding.inflate(inflater, container, false)
         return binding.root
     }
 
-    /**
-     * Called once the view has been created.
-     *
-     * Configure the button listeners and perform the first upload of certification data.
-     *
-     * @param view The root view of the fragment.
-     * @param savedInstanceState A Bundle containing data from the previous state of the fragment.
-     */
-
-    @RequiresApi(Build.VERSION_CODES.N)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        getEvents()
+        selectButton(binding.button1) // Seleccionar el botón de cursos
+        navigateToFragment(FragmentoEventos()) // Cargar el fragmento de cursos
 
-        binding.button3.setOnClickListener {
-            getCompanyCertification()
-
-            // Cambia el drawable y color del texto de button3
-            binding.button3.setBackgroundResource(R.drawable.button_active)
-            binding.button3.setTextColor(ContextCompat.getColor(requireContext(), R.color.white))
-
-            // Cambia el drawable y color del texto de button1
-            binding.button1.setBackgroundResource(R.drawable.button_inactive)
-            binding.button1.setTextColor(ContextCompat.getColor(requireContext(), R.color.black))
-            // Cambia el drawable y color del texto de button1
-            binding.button2.setBackgroundResource(R.drawable.button_inactive)
-            binding.button2.setTextColor(ContextCompat.getColor(requireContext(), R.color.black))
-        }
+        // Inicializar el estado de los botones
+        selectButton(binding.button1)
+        navigateToFragment(FragmentoEventos())
 
         binding.button1.setOnClickListener {
-            getEvents()
-
-            // Cambia el drawable y color del texto de button1
-            binding.button1.setBackgroundResource(R.drawable.button_active)
-            binding.button1.setTextColor(ContextCompat.getColor(requireContext(), R.color.white))
-
-            // Cambia el drawable y color del texto de button3
-            binding.button3.setBackgroundResource(R.drawable.button_inactive)
-            binding.button3.setTextColor(ContextCompat.getColor(requireContext(), R.color.black))
-            binding.button2.setBackgroundResource(R.drawable.button_inactive)
-            binding.button2.setTextColor(ContextCompat.getColor(requireContext(), R.color.black))
+            selectButton(binding.button1)
+            navigateToFragment(FragmentoEventos())
         }
+
         binding.button2.setOnClickListener {
-            handleButton2Click()
+            selectButton(binding.button2)
+            navigateToFragment(FragmentoProgramas())
         }
 
-    }
-    @RequiresApi(Build.VERSION_CODES.N)
-    private fun handleButton2Click() {
-        getPublication()
-
-        // Cambia el drawable y color del texto de button1
-        binding.button2.setBackgroundResource(R.drawable.button_active)
-        binding.button2.setTextColor(ContextCompat.getColor(requireContext(), R.color.white))
-
-        // Cambia el drawable y color del texto de button3
-        binding.button3.setBackgroundResource(R.drawable.button_inactive)
-        binding.button3.setTextColor(ContextCompat.getColor(requireContext(), R.color.black))
-        binding.button1.setBackgroundResource(R.drawable.button_inactive)
-        binding.button1.setTextColor(ContextCompat.getColor(requireContext(), R.color.black))
-    }
-
-
-    /**
-     * Obtain company certifications and update your view.
-     *
-     * Make an asynchronous request to obtain company certifications and, once obtained,
-     * Updates the list in the UI.
-     */
-
-    @RequiresApi(Build.VERSION_CODES.N)
-    private fun getCompanyCertification() {
-        showProgressBar()
-        CoroutineScope(Dispatchers.IO).launch {
-            try {
-                val companyCertificationRepository = companyCertificationRepository()
-                val result: CertificacionEmpresaObj? = companyCertificationRepository.getCompanyCertification(LoginActivity.token)
-
-
-                    if (result != null) {
-                        withContext(Dispatchers.Main) {
-                        val adapter = EmpresaAdapter(result.data.companies)
-                        binding.empresaList.adapter = adapter
-                        binding.empresaList.layoutManager = LinearLayoutManager(context)
-                    }
-                }
-            } catch (e: Exception) {
-                e.printStackTrace() // Log the exception
-            } finally {
-                withContext(Dispatchers.Main) {
-                    hideProgressBar()
-                }
-            }
+        binding.button3.setOnClickListener {
+            selectButton(binding.button3)
+            navigateToFragment(FragmentoCertificaciones())
         }
+
+
     }
 
-    @RequiresApi(Build.VERSION_CODES.N)
-    private fun getPublication() {
-        showProgressBar()
-        CoroutineScope(Dispatchers.IO).launch {
-            try {
-                val repositoryPublication = RepositoryPublication()
-                val result: PublicObjeto? = repositoryPublication.getPublication(LoginActivity.token)
-
-
-                if (result != null) {
-                    withContext(Dispatchers.Main) {
-                        val adapter = PublicationAdapter(result.data, onCommentClickListener)
-                        binding.empresaList.adapter = adapter
-                        binding.empresaList.layoutManager = LinearLayoutManager(context)
-                    }
-                }
-            } catch (e: Exception) {
-                e.printStackTrace() // Log the exception
-            } finally {
-                withContext(Dispatchers.Main) {
-                    hideProgressBar()
-                }
-            }
-        }
-    }
-    // Get events
-    @RequiresApi(Build.VERSION_CODES.N)
-    private fun getEvents() {
-        showProgressBar()
-        CoroutineScope(Dispatchers.IO).launch {
-            try {
-                val eventsRepository = EventRepository()
-                val result: EventObject? = eventsRepository.getEvent(LoginActivity.token)
-
-                if (result != null) {
-                   withContext(Dispatchers.Main) {
-                        val adapter = EventsAdapter(result.data.documents, onEventClickListener)
-                        binding.empresaList.adapter = adapter
-                        binding.empresaList.layoutManager = LinearLayoutManager(context)
-                    } 
-                }
-            }
-            catch (e: Exception) {
-                e.printStackTrace()
-            }
-            finally {
-                withContext(Dispatchers.Main) {
-                    hideProgressBar()
-                }
-            }
-        }
-    }
-    private fun showProgressBar() {
-        binding.progressBar.visibility = View.VISIBLE
-        binding.empresaList.visibility = View.INVISIBLE // Ocultar la lista mientras carga
+    private fun navigateToFragment(fragment: Fragment) {
+        parentFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .addToBackStack(null)
+            .commit()
     }
 
-    private fun hideProgressBar() {
-        binding.progressBar.visibility = View.GONE
-        binding.empresaList.visibility = View.VISIBLE
+    private fun selectButton(selectedButton: Button) {
+        // Desactivar todos los botones
+        binding.button1.isSelected = false
+        binding.button2.isSelected = false
+        binding.button3.isSelected = false
+
+        // Activar el botón seleccionado
+        selectedButton.isSelected = true
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+
 }

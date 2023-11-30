5a9c02405cdbd86db04b4ef107326576589464fc
unknown <urigopar889@gmail.com>
2023-11-29 19:26:20 -0600

    Likes finish

--- app/src/main/java/com/example/kotlin/robertoruizapp/framework/adapters/PublicationAdapter.kt
+++ app/src/main/java/com/example/kotlin/robertoruizapp/framework/adapters/PublicationAdapter.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.kotlin.robertoruizapp.R

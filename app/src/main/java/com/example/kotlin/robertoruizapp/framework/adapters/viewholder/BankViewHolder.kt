615a3d50e5cb2177b91c204c511e9402c1f328f4
OsvalDM <osvaldvm140303@gmail.com>
2023-11-30 19:46:57 -0600

    Add: Render imgs

--- app/src/main/java/com/example/kotlin/robertoruizapp/framework/adapters/viewholder/BankViewHolder.kt
+++ app/src/main/java/com/example/kotlin/robertoruizapp/framework/adapters/viewholder/BankViewHolder.kt
@@ -2,13 +2,34 @@ package com.example.kotlin.robertoruizapp.framework.adapters.viewholder
 
 import android.content.Context
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.Priority
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
 import com.example.kotlin.robertoruizapp.data.network.model.BankAccount.BankDocument
 import com.example.kotlin.robertoruizapp.databinding.ItemBankBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class BankViewHolder(private val binding: ItemBankBinding) : RecyclerView.ViewHolder(binding.root) {
     fun bind(item: BankDocument, context: Context){
         binding.cuentaCurso.text = item.bank
-        binding.cursoCosto.text = item.accountNumber.toString()
+        binding.cursoCosto.text = item.accountNumber
+
+        CoroutineScope(Dispatchers.Main).launch {
+            val urlImage = item.bankImage
+
+            val requestOptions =  RequestOptions()
+                .centerCrop()
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .fitCenter()
+                .priority(Priority.HIGH)
+
+            Glide.with(context).load(urlImage)
+                .apply(requestOptions)
+                .into(binding.imagenBanco)
+        }
     }
 
 }
